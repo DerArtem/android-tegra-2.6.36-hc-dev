c952cda364520070387523f89df64c58693f899d
Artem Makhutov <artem@makhutov.org>
2011-12-11 20:32:58 +0100

    Added some more debug from I2C

--- drivers/i2c/busses/i2c-tegra.c
+++ drivers/i2c/busses/i2c-tegra.c
@@ -673,7 +673,7 @@ static u32 tegra_i2c_func(struct i2c_adapter *adap)
 {
 	/* FIXME: For now keep it simple and don't support protocol mangling
 	   features */
-	return I2C_FUNC_I2C | I2C_FUNC_SMBUS_BYTE | I2C_FUNC_SMBUS_BYTE_DATA;
+	return I2C_FUNC_I2C | I2C_FUNC_SMBUS_BYTE | I2C_FUNC_SMBUS_BYTE_DATA | I2C_FUNC_SMBUS_EMUL;
 }
 
 static const struct i2c_algorithm tegra_i2c_algo = {
