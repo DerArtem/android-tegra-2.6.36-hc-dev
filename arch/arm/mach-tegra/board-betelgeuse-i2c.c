7099b92537887dae2869fc6a012a6c440860e281
Artem Makhutov <artem@makhutov.org>
2011-12-29 14:34:03 +0100

    Some I2C tests

--- arch/arm/mach-tegra/board-betelgeuse-i2c.c
+++ arch/arm/mach-tegra/board-betelgeuse-i2c.c
@@ -43,6 +43,7 @@ static struct tegra_i2c_platform_data betelgeuse_i2c1_platform_data = {
 	.bus_clk_rate	= { 100000, 0 },
 };
 
+/*
 static const struct tegra_pingroup_config i2c2_ddc = {
 	.pingroup	= TEGRA_PINGROUP_DDC,
 	.func		= TEGRA_MUX_I2C2,
@@ -60,14 +61,13 @@ static struct tegra_i2c_platform_data betelgeuse_i2c2_platform_data = {
 	.bus_mux	= { &i2c2_ddc, &i2c2_gen2 },
 	.bus_mux_len	= { 1, 1 },
 };
+*/
 
-/*
-static struct tegra_i2c_platform_data betelgeuse_i2c3_platform_data = {
-	.adapter_nr	= 3,
+static struct tegra_i2c_platform_data betelgeuse_i2c2_platform_data = {
+	.adapter_nr	= 1,
 	.bus_count	= 1,
 	.bus_clk_rate	= { 100000, 0 },
 };
-*/
 
 static struct tegra_i2c_platform_data betelgeuse_dvc_platform_data = {
 	.adapter_nr	= 4,
