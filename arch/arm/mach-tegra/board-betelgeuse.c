93cd1fb00dfb736f9a3428a917f10e9c6409ab35
Artem Makhutov <artem@makhutov.org>
2011-12-30 16:51:46 +0100

    Cleanup - the Folio does not use GPIO Keys

--- arch/arm/mach-tegra/board-betelgeuse.c
+++ arch/arm/mach-tegra/board-betelgeuse.c
@@ -27,7 +27,6 @@
 #include <linux/i2c.h>
 #include <linux/i2c-tegra.h>
 #include <linux/gpio.h>
-#include <linux/gpio_keys.h>
 #include <linux/input/eeti_ts.h>
 #include <linux/input.h>
 #include <linux/io.h>
@@ -127,142 +126,9 @@ static void __init tegra_betelgeuse_fixup(struct machine_desc *desc,
 	mi->bank[0].size  = SHUTTLE_MEM_SIZE - SHUTTLE_GPU_MEM_SIZE;
 }
 
-/*
-static struct tegra_suspend_platform_data betelgeuse_suspend = {
-	.cpu_timer = 5000,
-	.cpu_off_timer = 5000,
-	.core_timer = 0x7e7e,
-	.core_off_timer = 0x7f,
-	.separate_req = true,
-	.corereq_high = false,
-	.sysclkreq_high = true,
-	.suspend_mode = TEGRA_SUSPEND_LP0,
-};
-*/
-
-static struct tegra_suspend_platform_data betelgeuse_suspend = {
-        /*
-         * Check power on time and crystal oscillator start time
-         * for appropriate settings.
-         */
-        .cpu_timer      = 2000,
-        .cpu_off_timer  = 100,
-        .suspend_mode   = TEGRA_SUSPEND_LP0,
-        .core_timer     = 0x7e7e,
-        .core_off_timer = 0xf,
-        .separate_req   = true,
-        .corereq_high   = false,
-        .sysclkreq_high = true,
-        .wake_enb       = TEGRA_WAKE_GPIO_PV2,
-        .wake_high      = 0,
-        .wake_low       = TEGRA_WAKE_GPIO_PV2,
-        .wake_any       = 0,
-};
-
-/*
-static struct tegra_suspend_platform_data shuttle_suspend = {
-        .cpu_timer = 2000, // 5000
-        .cpu_off_timer = 0, // 5000
-        .core_timer = 0x7e7e, //
-        .core_off_timer = 0, // 0x7f
-        .corereq_high = false,
-        .sysclkreq_high = true,
-        .suspend_mode = TEGRA_SUSPEND_LP0,
-        #if LINUX_VERSION_CODE <= KERNEL_VERSION(2,6,38) //NB: 2.6.39+ handles this automatically
-        .separate_req = true,
-        .wake_enb = SHUTTLE_WAKE_KEY_POWER | SHUTTLE_WAKE_KEY_RESUME | TEGRA_WAKE_RTC_ALARM,
-        .wake_low = SHUTTLE_WAKE_KEY_POWER | SHUTTLE_WAKE_KEY_RESUME,
-        .wake_any = 0,
-        #endif
-};*/
-
-#ifdef CONFIG_KEYBOARD_GPIO
-#define GPIO_KEY(_id, _gpio, _iswake) \
-{ \
-.code = _id, \
-.gpio = TEGRA_GPIO_##_gpio, \
-.active_low = 1, \
-.desc = #_id, \
-.type = EV_KEY, \
-.wakeup = _iswake, \
-.debounce_interval = 10, \
-}
-
-static struct gpio_keys_button antares_keys[] = {
-	[0] = GPIO_KEY(KEY_VOLUMEUP, PQ5, 0),
-	[1] = GPIO_KEY(KEY_VOLUMEDOWN, PQ4, 0),
-	[2] = GPIO_KEY(KEY_POWER, PV2, 1),
-};
-
-#define PMC_WAKE_STATUS 0x14
-
-static int antares_wakeup_key(void)
-{
-	unsigned long status = readl(IO_ADDRESS(TEGRA_PMC_BASE) + PMC_WAKE_STATUS);
-	writel(0xffffffff, IO_ADDRESS(TEGRA_PMC_BASE) + PMC_WAKE_STATUS);
-	if (status & TEGRA_WAKE_GPIO_PV2) /* power button */
-        	return KEY_POWER;
-	else if (status & TEGRA_WAKE_GPIO_PV3) /* AC adapter plug in/out */
-        	return KEY_POWER;
-	else
-		return KEY_RESERVED;
-}
-
-static struct gpio_keys_platform_data antares_keys_platform_data = {
-	.buttons = antares_keys,
-	.nbuttons = ARRAY_SIZE(antares_keys),
-	.wakeup_key = antares_wakeup_key,
-};
-
-static struct platform_device antares_keys_device = {
-	.name = "gpio-keys",
-	.id = 0,
-	.dev = {
-		.platform_data = &antares_keys_platform_data,
-	},
-};
-
-static void antares_keys_init(void)
-{
-	int i;
-	for (i = 0; i < ARRAY_SIZE(antares_keys); i++)
-	tegra_gpio_enable(antares_keys[i].gpio);
-}
-#endif
-
-/*
-static struct gpio_keys_button betelgeuse_gpio_keys_buttons[] = {
-	{
-		.code		= KEY_POWER,
-		.gpio		= TEGRA_GPIO_PA0,
-		.active_low	= 1,
-		.desc		= "Power",
-		.type		= EV_KEY,
-		.wakeup		= 1,
-	},
-};
-
-static struct gpio_keys_platform_data betelgeuse_gpio_keys = {
-	.buttons	= betelgeuse_gpio_keys_buttons,
-	.nbuttons	= ARRAY_SIZE(betelgeuse_gpio_keys_buttons),
-};
-
-static struct platform_device betelgeuse_gpio_keys_device = {
-	.name	= "gpio-keys",
-	.id	= -1,
-	.dev	= {
-		.platform_data = &betelgeuse_gpio_keys,
-	},
-};
-*/
-
 static struct platform_device *betelgeuse_devices[] __initdata = {
         &debug_uart,
         &tegra_udc_device,
-        //&betelgeuse_gpio_keys_device,
-	#ifdef CONFIG_KEYBOARD_GPIO
-	&antares_keys_device,
-	#endif
         &tegra_spi_device1,
         &tegra_spi_device2,
         &tegra_spi_device3,
@@ -298,9 +164,6 @@ static void __init tegra_betelgeuse_init(void)
 	platform_add_devices(betelgeuse_devices, ARRAY_SIZE(betelgeuse_devices));
 
 	//betelgeuse_kbc_init();
-	#ifdef CONFIG_KEYBOARD_GPIO
-	antares_keys_init();
-	#endif
 	betelgeuse_touch_init_egalax();
 	//antares_ec_init();
 	betelgeuse_wifi_init();
